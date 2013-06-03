02509a9df7c5d08308f72521b4441686d4da5a85
Shane Buckner <crickindaneck@hotmail.com>
2013-06-03 15:29:15 -0500

    File Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CSharpVisualPractice_001
+{
+    partial class myWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(myWindow));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuOptions = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuOptions});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuOptions
+            // 
+            this.menuOptions.Name = "menuOptions";
+            this.menuOptions.Size = new System.Drawing.Size(80, 20);
+            this.menuOptions.Text = "Main Menu";
+            this.menuOptions.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // myWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "myWindow";
+            this.Text = "Formula For Disaster";
+            this.Load += new System.EventHandler(this.myWindow_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuOptions;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpVisualPractice_001
+{
+    public partial class myWindow : Form
+    {
+        Bitmap myBitmap;
+        
+        public myWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onExit(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            MainMenu menu = new MainMenu();
+            MenuItem item = menu.MenuItems.Add("&Options");
+            item.MenuItems.Add(new MenuItem("&Open...", new EventHandler(OnOpenImage), Shortcut.CtrlO));
+            item.MenuItems.Add("-----");
+            item.MenuItems.Add(new MenuItem("E&xit", new EventHandler(onExit)));
+
+            // Attach the menu to the form
+            Menu = menu;
+        }
+
+        private void OnOpenImage(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = "C:\\Users\\Shane\\Libraries\\Documents";
+            ofd.Filter = "Image Files (JPEG, GIF, BMP, etc.)|" +
+                "*.jpg;*.jpeg;*.gif;*.bmp;*.tif;*.tiff;*.png|" +
+                "JPEG files (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                "GIF Files (*.gif)|*.gif|" +
+                "BMP Files (*.bmp)|*.bmp|" +
+                "TIFF Files (*.tif;*.tiff)|*.tif;*.tiff|" +
+                "PNG Files (*.png)|*.png|" +
+                "All files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                String fileName = ofd.FileName;
+                if (fileName.Length != 0)
+                {
+                    try
+                    {
+                        myBitmap = new Bitmap(fileName);
+                        Text = "Image Viewer - " + fileName;
+                        Invalidate();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+                if (myBitmap != null)
+                {
+                    Graphics g = e.Graphics;
+
+                    ClientSize = new Size(myBitmap.Width + 10, myBitmap.Height + 10);
+
+                    g.DrawImage(myBitmap, 0, 0,
+                        myBitmap.Width, myBitmap.Height);
+                }
+        }
+    }
+}
